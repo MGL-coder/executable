20ea06cb9e77c3cc43f185bb27f1711c5674e9dc
MGL-coder <madiyar.galymgereyev@gmail.com>
2021-06-08 21:44:29 +0600

    Changes for v1.0.1

--- executable.go
+++ executable.go
@@ -11,5 +11,9 @@ func main () {
 
 	fmt.Print("Changing the case of \"USA\" string:")
 	fmt.Println(library.ChangeCase("USA"))
+
+	x1, x2, _ := library.ToSolveQuadraticFunc(1, 5, 3)
+	fmt.Print("The solution for the \"x^2 + 5x + 3 = 0\" is ")
+	fmt.Printf("x1 = %v, x2 = %v\n", x1, x2)
 }
 
