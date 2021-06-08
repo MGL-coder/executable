f012f4739d799da0fed00ef3f1431f940988441b
MGL-coder <madiyar.galymgereyev@gmail.com>
2021-06-08 21:46:29 +0600

    Changes for v1.0.2

--- executable.go
+++ executable.go
@@ -15,5 +15,8 @@ func main () {
 	x1, x2, _ := library.ToSolveQuadraticFunc(1, 5, 3)
 	fmt.Print("The solution for the \"x^2 + 5x + 3 = 0\" is ")
 	fmt.Printf("x1 = %v, x2 = %v\n", x1, x2)
+
+	fmt.Println("The UUID:")
+	fmt.Println(library.ToGetUUID())
 }
 
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/MGL-coder/executable
 
 go 1.16
 
-require github.com/MGL-coder/library v1.0.1
+require github.com/MGL-coder/library v1.0.2
